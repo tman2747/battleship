01ea0e6fac7a2c07f23cc37a137ab99f16b494e6
Triston Land <tristonland@gmail.com>
2025-04-29 23:42:07 -0600

    add game loop and fix a ton of bugs

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import "./styles.css";
 
 const Player = require("./player");
 
+let player1 = new Player();
+player1.showShip = true;
+let player2 = new Player();
+let gameover = false;
+
 const gameContainer = document.querySelector(".gameContainer");
 const shuffleButton = document.querySelector(".play");
 shuffleButton.addEventListener("click", () => {
 	shuffleButton.innerHTML = "shuffle";
 	shuffleButton.classList.remove("play");
 	shuffleButton.classList.add("shuffle");
+
 	shuffle();
 });
 
@@ -17,25 +23,33 @@ function makeTiles(player) {
 	playerContainer.classList.add("playerContainer");
 	let playerGrid = document.createElement("div");
 	playerGrid.classList.add("playerGrid");
-	player.gameboard.board.forEach((element) => {
-		element.forEach((element) => {
+	player.gameboard.board.forEach((element, x) => {
+		element.forEach((element, y) => {
 			let tile = document.createElement("div");
 			tile.classList.add("tile");
 			if (player.showShip == true && element.ship) {
 				tile.innerHTML = "X";
 			}
-			else if(element.ship)
-			{
-				tile.innerHTML = "X";
-
+			element.domTile = tile;
+			if (player.showShip == false) {
+				tile.addEventListener("click", () => {
+					if (!gameover && !player.gameboard.board[x][y].attacked) {
+						player.gameboard.receiveAttack(x, y);
+						checkgameover();
+						const pos = pickRandomPos(storedPostions);
+						if (!gameover) {
+							if (pos !== null) {
+								player1.gameboard.receiveAttack(...pos);
+								checkgameover();
+							} else {
+								console.log("No available positions left");
+							}
+							console.log(player1.gameboard.ships);
+						}
+					}
+				});
 			}
-			tile.addEventListener("click", () => {
-				if (element.ship != null) {
-					tile.style.background = "green";
-				} else {
-					tile.style.background = "red";
-				}
-			});
+			console.log(element.domTile);
 			playerGrid.appendChild(tile);
 		});
 	});
@@ -43,10 +57,20 @@ function makeTiles(player) {
 	gameContainer.appendChild(playerContainer);
 }
 
+function checkgameover() {
+	if (player1.gameboard.allShipsSunk()) {
+		gameover = true;
+	}
+	if (player2.gameboard.allShipsSunk()) {
+		gameover = true;
+	}
+}
 function shuffle() {
-	let player1 = new Player();
+	player1 = new Player();
 	player1.showShip = true;
-	let player2 = new Player();
+	player2 = new Player();
+	storedPostions = [];
+	gameover = false;
 	gameContainer.innerHTML = "";
 	placeShips(player1);
 	placeShips(player2);
@@ -55,31 +79,19 @@ function shuffle() {
 }
 
 function placeShips(player) {
-	let amountOfShips = 5;
-	let maxLength = 5;
-	for (let index = 0; index < amountOfShips; index++) {
-		// add length to get rid of magic number
-		let firstRandom = Math.floor(Math.random() * 5); // TODO add gameboard width and height to get rid of magic number
-		let secondRandom = Math.floor(Math.random() * 5); // TODO add gameboard width and height to get rid of magic number
-		let placed = player.gameboard.placeShip(
-			firstRandom,
-			secondRandom,
-			true,
-			4,
-		);
-		if (!placed) {
-			let placedsuc = false;
-			while (!placedsuc) {
-				let firstRandom = Math.floor(Math.random() * 5); // TODO add gameboard width and height to get rid of magic number
-				let secondRandom = Math.floor(Math.random() * 5); // TODO add gameboard width and height to get rid of magic number
-				let length = Math.floor(Math.random() * maxLength + 1); // TODO totally rewrite this
-				placedsuc = player.gameboard.placeShip(
-					firstRandom,
-					secondRandom,
-					false,
-					length, // TODO add length to get rid of magic number
-				);
-			}
+	const amountOfShips = 3;
+	const maxLength = 4;
+	let placedShips = 0;
+
+	while (placedShips < amountOfShips) {
+		const x = Math.floor(Math.random() * 5);
+		const y = Math.floor(Math.random() * 5);
+		const length = 3; // Keep consistent ship length for now
+		const isHorizontal = Math.random() < 0.5;
+
+		const placed = player.gameboard.placeShip(x, y, isHorizontal, length);
+		if (placed) {
+			placedShips++;
 		}
 	}
 }
@@ -91,12 +103,13 @@ function placeShips(player) {
 function pickRandomPos(storedPostions) {
 	let x, y;
 
-	if (storedPostions == undefined) {
+	if (!storedPostions) {
 		x = Math.floor(Math.random() * 5);
 		y = Math.floor(Math.random() * 5);
 		return [x, y];
 	}
-	if (storedPostions.length >= 25) {
+
+	if (storedPostions.length === 25) {
 		console.log("All positions taken");
 		return null;
 	}
@@ -104,6 +117,7 @@ function pickRandomPos(storedPostions) {
 	do {
 		x = Math.floor(Math.random() * 5);
 		y = Math.floor(Math.random() * 5);
+		console.log(x, y);
 	} while (storedPostions.some((pos) => pos[0] === x && pos[1] === y));
 
 	storedPostions.push([x, y]);
@@ -111,4 +125,3 @@ function pickRandomPos(storedPostions) {
 }
 
 let storedPostions = [];
-pickRandomPos(storedPostions);
